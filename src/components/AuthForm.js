f599d1222ef4ba3312ba3cf814d440395cb29cdd
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-27 13:13:41 -0500

    created reusable auth form

---
+++ src/components/AuthForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Button, Input } from 'react-native-elements';
+
+const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	return (
+		<React.Fragment>
+			<Spacer>
+				<Text h3>Sign Up for Tracker</Text>
+			</Spacer>
+			<Input label="Email" value={email} onChangeText={setEmail} autoCapitalize="none" autoCorrect={false} />
+			<Spacer />
+			<Input
+				secureTextEntry
+				label="Password"
+				value={password}
+				onChangeText={setPassword}
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
+			{state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
+			<Spacer>
+				<Button title="Sign up" onPress={() => signup({ email, password })} />
+			</Spacer>
+		</React.Fragment>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthForm;
