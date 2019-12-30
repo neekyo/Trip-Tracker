97f3cf5690bebcc25b421968d0aafa1e4a15b94b
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-30 17:57:06 -0500

    handle clearing error messages between sign up & sign in screens from persisiting

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,21 +1,23 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 import { Context } from '../context/AuthContext';
 
 const SigninScreen = () => {
-	const { state, signin } = useContext(Context);
+	const { state, signin, clearErrorMessage } = useContext(Context);
 
 	return (
 		<View style={styles.container}>
+			<NavigationEvents onWillBlur={clearErrorMessage} />
 			<AuthForm
 				headerText="Sign in to your Account"
 				errorMessage={state.errorMessage}
 				onSubmit={signin}
 				submitButtonText="Sign in"
 			/>
-			<NavLink text="Don't have an account? Sign up instead!" routeName="Sign up" />
+			<NavLink text="Don't have an account? Sign up instead!" routeName="Signup" />
 		</View>
 	);
 };
