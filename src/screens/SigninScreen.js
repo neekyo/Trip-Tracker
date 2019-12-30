e165f897f7e36fe0bd72557834982e70b1aa68c3
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-30 17:25:45 -0500

    wired up sign in screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import { Context } from '../context/AuthContext';
 
 const SigninScreen = () => {
-	return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+	const { state, signin } = useContext(Context);
+
+	return (
+		<View style={styles.container}>
+			<AuthForm
+				headerText="Sign in to your Account"
+				errorMessage={state.errorMessage}
+				onSubmit={signin}
+				submitButtonText="Sign in"
+			/>
+			<NavLink text="Don't have an account? Sign up instead!" routeName="Sign up" />
+		</View>
+	);
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = {
+	header: null
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		marginBottom: 250
+	}
+});
 
 export default SigninScreen;
