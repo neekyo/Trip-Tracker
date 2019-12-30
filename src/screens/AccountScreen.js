f42a22b5b3d8c5f9b5fa4c4ec721f698f97022f1
Nick Sherman <ogleffeckt2@yahoo.com>
2019-12-30 18:48:57 -0500

    initialized create track screen & displayed a world map

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,19 +1,20 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import { SafeAreaView } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
+import Spacer from '../components/Spacer';
 
 const AccountScreen = () => {
 	const { signout } = useContext(AuthContext);
 
 	return (
-		<React.Fragment>
+		<SafeAreaView forceInset={{ top: 'always' }}>
 			<Text style={{ fontSize: 48 }}>AccountScreen</Text>
 			<Spacer>
 				<Button title="Sign out" onPress={signout} />
 			</Spacer>
-		</React.Fragment>
+		</SafeAreaView>
 	);
 };
 
