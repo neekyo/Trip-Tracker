cf29653e2a783beae47043f58017156603259c1f
Nick Sherman <ogleffeckt2@yahoo.com>
2020-01-04 19:30:30 -0500

    fixing odds & ends - just styling left!

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { Context as AuthContext } from '../context/AuthContext';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
 	const { signout } = useContext(AuthContext);
@@ -12,10 +13,15 @@ const AccountScreen = () => {
 		<SafeAreaView forceInset={{ top: 'always' }}>
 			<Text style={{ fontSize: 48 }}>AccountScreen</Text>
 			<Spacer>
-				<Button title="Sign out" onPress={signout} />
+				<Button title="Sign Out" onPress={signout} />
 			</Spacer>
 		</SafeAreaView>
 	);
+};
+
+AccountScreen.navigationOptions = {
+	title: 'Account',
+	tabBarIcon: <FontAwesome name="gear" size={20} />
 };
 
 const styles = StyleSheet.create({});
